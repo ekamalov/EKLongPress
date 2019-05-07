c1548d62c8111361aa93a9036e73d42cf3c2c37c
Erik Kamalov <ekamalov967@gmail.com>
2019-05-07 17:09:41 +0600

    added calculations methods

--- EKLongTouch/EKContextMenu/EKContextMenuView.swift
+++ EKLongTouch/EKContextMenu/EKContextMenuView.swift
@@ -63,10 +63,62 @@ class EKContextMenuView: UIView{
 
 // MARK: - private methods
 extension EKContextMenuView {
+    private func negativeQuorterAngle(start angle: CGFloat) {
+        for (index,item) in  properties.items.enumerated() {
+            item.angle = (angle - CGFloat(45 * index))
+        }
+    }
+    
+    private func positiveQuorterAngle(start angle: CGFloat) {
+        for (index,item) in  properties.items.enumerated() {
+            item.angle = (angle + CGFloat(45 * index))
+        }
+    }
+    private func calculatePointCoordiantes(_ angle: CGFloat) -> CGPoint {
+        let x = touchPoint.x + cos(.pi * angle/180) * (itemDistance.x)
+        let y = touchPoint.y + sin(.pi * angle/180) * (itemDistance.y)
+        return CGPoint(x: x, y: y)
+    }
+    
+    /// Calculates which angle the menu items should appear
+    private func anglesForDirection() {
+        let directions = calculateDirections()
+        switch directions{
+        case (.down, .right):
+            positiveQuorterAngle(start: 0)
+        case (.down, .middle):
+            positiveQuorterAngle(start: 90)
+        case (.middle, .right):
+            positiveQuorterAngle(start: 270)
+        case (.down, .left):
+            negativeQuorterAngle(start: 180)
+        case (.up, .right):
+            negativeQuorterAngle(start: 0)
+        case (.up, .middle), (.up, .left), (.middle,.middle):
+            positiveQuorterAngle(start: 180)
+        case (.middle, .left):
+            positiveQuorterAngle(start: 135)
+        default: break
+        }
+    }
+    
+    private func itemSize()-> CGSize {
+        guard let size = properties.items.first?.appearance.size else {
+            return EKContextMenuItemAppearance().size
+        }
+        return size
+    }
+    
+    /// Calculates the distance from the user's touch location to the menu items
+    private func calculateDistanceToItem() {
+        self.itemDistance.x = ((touchPointView.frame.width + itemSize().width) / 2) + distanceToTouchPointView
+        self.itemDistance.y = ((touchPointView.frame.height + itemSize().height) / 2) + distanceToTouchPointView
+    }
+    
     private func calculateDirections() -> (vertical:Direction, horizontal:Direction) {
         var direction:(vertical:Direction, horizontal:Direction) =  (vertical: .middle, horizontal: .middle)
         
-        let size:CGFloat = (distanceToTouchPointView + touchPointView.frame.width) + (properties.items.first?.appearance.size.width ?? 30)
+        let size:CGFloat = (distanceToTouchPointView + touchPointView.frame.width) + itemSize().width
         let touchPoint = touchPointView.frame.origin
         
         if touchPoint.y + size > UIScreen.main.bounds.height {
