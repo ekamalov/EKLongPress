a46250c6b3ff027d90d9a4dfedf853ef79921232
Erik Kamalov <ekamalov967@gmail.com>
2019-05-07 17:08:24 +0600

    added animation properties

--- EKLongTouch/EKContextMenu/EKContextAppearance.swift
+++ EKLongTouch/EKContextMenu/EKContextAppearance.swift
@@ -31,16 +31,16 @@ public struct EKTouchPointApperance:EKBuilder {
     var size: CGSize = .init(width: 45, height: 45)
     
     /// The size of the touch location view // default value 3
-    var borderWidth:CGFloat = 5
+    var borderWidth:CGFloat = 7
 }
 
 public struct EKContextAppearance:EKBuilder {
     public init() {}
     
-    /// The background's alpha of the view. // defaul value 0.9
+    /// The background's alpha of the view. // default value 0.9
     var backgroundAlpha: CGFloat = 0.9
     
-    /// The background's colour of the view // defaul black
+    /// The background's colour of the view // default black
     var backgroundColor: UIColor = .black
     
 }
@@ -57,8 +57,14 @@ public struct EKContextMenuItemAppearance: EKBuilder {
     /// The size of the item view // default 35
     var size: CGSize = .init(width: 45, height: 45)
     
-    /// The background's colour of the view // defaul black
+    /// The background's colour of the view // default black
     var backgroundColor: UIColor = .black
+    
+    
+    var dampingRatio:CGFloat = 0.4
+    var duration:Double = 1
+    var delay:Double = 0.3
+    
 }
 
 
