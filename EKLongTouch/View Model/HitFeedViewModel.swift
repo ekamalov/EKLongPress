23331cd908629841b85963b44a2c1bc3fe440f2e
Erik Kamalov <ekamalov967@gmail.com>
2019-06-14 19:00:28 +0600

    added assets for example

---
+++ EKLongTouch/View Model/HitFeedViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  HitFeedViewModel.swift
+//  EKLongTouch
+//
+//  Created by Erik Kamalov on 6/13/19.
+//  Copyright © 2019 E K. All rights reserved.
+//
+
+import Foundation
---
+++ EKLongTouch/View/HitFeedHeader.swift
@@ -0,0 +1,9 @@
+//
+//  HitFeedHeader.swift
+//  EKLongTouch
+//
+//  Created by Erik Kamalov on 6/14/19.
+//  Copyright © 2019 E K. All rights reserved.
+//
+
+import Foundation
